8e0fc444f14ce0b8f1a8a271b9cbc332bbfca808
kaloyanTry <kaloyantryavna@gmail.com>
2026-07-27 09:24:32 +0300

    Footer refactored

--- src/i18n/bg.ts
+++ src/i18n/bg.ts
@@ -5,6 +5,13 @@ const bg = {
     trails: 'Пътеки',
     events: 'Събития',
   },
+
+  footer: {
+    resources: 'Полезни връзки',
+    community: 'Общност',
+    contact: 'Контакти',
+    copyright: '© 2026 TRAVNA · Съдържанието е лицензирано под',
+  },
 };
 
 export default bg;
--- src/i18n/en.ts
+++ src/i18n/en.ts
@@ -5,6 +5,13 @@ const en = {
     trails: 'Trails',
     events: 'Events',
   },
+
+  footer: {
+    resources: 'Resources',
+    community: 'Community',
+    contact: 'Contact',
+    copyright: '© 2026 TRAVNA · Content licensed under',
+  },
 };
 
 export default en;
